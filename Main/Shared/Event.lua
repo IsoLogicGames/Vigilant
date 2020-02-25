057d2bf7579eb47c035526302b7392b4582e1241
LastTalon <last_talon@new.rr.com>
2020-02-25 15:31:51 -0600

    Add Event

--- Main/Shared/Event.lua
+++ Main/Shared/Event.lua
@@ -8,7 +8,30 @@ Console.log("Initializing locals...")
 
 function Event.new()
 	local self = setmetatable({}, Event)
+	self.listeners = {}
+	self.Registrar = {}
+	self.Registrar.listeners = self.listeners
+	self.Registrar.Connect = self.Connect
+	self.Registrar.Disconnect = self.Disconnect
+	self.Activator = {}
+	self.Activator.listeners = self.listeners
+	self.Activator.Fire = self.Fire
 	return self
+end
+
+function Event:Connect(fn)
+	table.insert(self.listeners, fn)
+	return #self.listeners
+end
+
+function Event:Disconnect(id)
+	table.remove(self.listeners, id)
+end
+
+function Event:Fire(...)
+	for _, fn in ipairs(self.listeners) do
+		fn(...)
+	end
 end
 
 Console.log("Initialized.")
